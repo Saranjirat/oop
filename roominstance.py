c58445c33f719803a2c36346540d169dfffd93e4
johnybravou <128812874+johnybravou2@users.noreply.github.com>
2023-04-14 17:05:30 +0700

    roominstance

--- roominstance.py
+++ roominstance.py
@@ -1,72 +1,29 @@
-from room import Room
-#kirimaya resort
-room_plantationview = Room("Kirimayaresort",
-                           "Plantation View",
-                           "42 sq. m.",
-                           "1 Bedroom",
-                           "1 Room",
-                           [],
-                           "1000")
-                
+from hotelandroom import Hotel
+from hotelandroom import Room
 
-room_horizonview = Room("Kirimayaresort",
-                        "Horizon View",
-                        "42 sq. m.",
-                        "1 Bedroom",
-                        "1 Room",
-                        [],
-                        "2000")
+kirimaya = Hotel()
+##kiri
 
-room_terrace_suite = Room("Kirimayaresort",
-                          "Terrace Suite",
-                          "84 sq. m.",
-                          "1 Bedroom",
-                          "1 Room",
-                          [],
-                          "2500")
+room_plantationview = Room("Plantation View","42 sq. m.","1 Bedroom","1 Room", "3 Persons(2 Adults + 1 Child)", "Double or Twin",
+                           "-Balcony daybed with a view,-Cable TV,-LCD screen in Suites and Villas,DVD/ CD players")
+
+room_horizonview = Room("Horizon View","42 sq. m.","1 Bedroom","1 Room","3 Persons (2 Adults + 1 Child)","Double or Twin",
+                        "-Balcony daybed with a view,-Cable TV,-LCD screen in Suites and Villas,-DVD/ CD players")
+
+room_terrace_suite = Room("Terrace Suite","84 sq. m.","1 Bedroom","1 Room","3 Persons (2 Adults + 1 Child)","Double or Twin")
 
 
 ##muthi
 
-room_muthimaya_forest_poolvilla = Room("Muthimaya",
-                                        "MUTHI MAYA Forest Pool Villa",
-                                       "164 sq. m.",
-                                       "1 Bedroom",
-                                       "1 Room",
-                                       [],
-                                       "2500")
+room_muthimaya_forest_poolvilla = Room("MUTHI MAYA Forest Pool Villa","164 sq. m.","1 Bedroom","1 Room"," 3 Persons (2 Adults + 1 Child)","Double Bed")
 
 
 ##atta
 
-room_one_bedroom_suite = Room("Atta",
-                              "One Bedroom Suite",
-                              "65 sq. m.",
-                              "1 Bedroom",
-                              "1 Room",
-                              [],
-                              "2000")
+room_one_bedroom_suite = Room("One Bedroom Suite","65 sq. m.","1 Bedroom","1 Room")
 
-room_one_bedroom_delight = Room("Atta",
-                                "One Bedroom Delight",
-                                "65 sq. m.",
-                                "1 Bedroom",
-                                "1 Room",
-                                [],
-                                "2000")
+room_one_bedroom_delight = Room("One Bedroom Delight","65 sq. m.","1 Bedroom","1 Room")
 
-room_two_bedroom_delight = Room("Atta",
-                                "Two Bedroom Delight",
-                                "102 sq. m.",
-                                "2 Bedroom",
-                                "2 Room",
-                                [],
-                                "2500")
+room_two_bedroom_delight = Room("Two Bedroom Delight","102 sq. m.","2 Bedroom","2 Room")
 
-room_penthouse_suite = Room("Atta",
-                            "Penthouse Suite",
-                            "235 sq. m.",
-                            "2 Bedroom",
-                            "2 Room",
-                            [],
-                            "2000")
+room_penthouse_suite = Room("Penthouse Suite","235 sq. m.","2 Bedroom","2 Room")
